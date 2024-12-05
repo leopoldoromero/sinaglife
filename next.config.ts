82a3f2c272ab3d58cb5d74ee90c2b948214f0c53
Leopoldo <nicoromerom87@gmail.com>
2024-12-05 19:52:12 +0100

    removed harcoded host  from next config

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,17 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
-  images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '41029007.servicio-online.net',
-        port: '',
-        pathname: 'wp-content/uploads/**',
-      },
-    ], 
-  },
+  // images: {
+  //   formats: ["image/avif", "image/webp"],
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: '',
+  //       port: '',
+  //       pathname: 'wp-content/uploads/**',
+  //     },
+  //   ], 
+  // },
 };
 
 export default nextConfig;
